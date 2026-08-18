acd433d67a5c1ff96dabc0c80e223e54d351f640
kishan <mekishan13@gmail.com>
2026-08-18 23:37:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
 import joblib
 
 app = Flask(__name__, static_folder='static')
+
+CORS(app)
 
 model = joblib.load('boston_prediction')
 
@@ -11,19 +14,39 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    data = request.get_json()
+
+    rm = float(data['rm'])
+    pt = float(data['pt'])
+    lstat = float(data['lstat'])
+
+    prediction = model.predict([[rm, pt, lstat]])
+
+    return jsonify({
+        'prediction': float(prediction[0]),
+        'rm': rm,
+        'pt': pt,
+        'lstat': lstat
+    })
+
+
 @app.route('/index', methods=['GET', 'POST'])
 def get_values():
 
     if request.method == 'POST':
+
         rm = float(request.form.get('rm'))
         pt = float(request.form.get('pt'))
         lstat = float(request.form.get('lstat'))
 
-        predict_value = model.predict([[rm, pt, lstat]])
+        prediction = model.predict([[rm, pt, lstat]])
 
         return render_template(
             'result.html',
-            prediction=predict_value[0],
+            prediction=prediction[0],
             rm=rm,
             pt=pt,
             lstat=lstat
@@ -32,5 +55,5 @@ def get_values():
     return render_template('index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
